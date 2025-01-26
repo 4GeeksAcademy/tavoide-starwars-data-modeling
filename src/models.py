a345b3c1d8db113d450fa90f69915a077968eee0
Tavoide <gustavobenitez116@gmail.com>
2025-01-26 03:14:26 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -7,23 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(15), nullable=False)
+    apellido = Column(String(15), nullable=False)
+    email = Column(String(20), unique=True, nullable=False)
+    password =  Column(String(10), nullable=False)
+    favoritos= relationship('Favoritos', back_populates="usuario", cascade="all, delete-orphan")
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planeta(Base):
+    __tablename__ = 'planeta'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(20),unique=True, nullable=False)
+    clima = Column(String(50), nullable=False)
+    residentes = Column(String(50), nullable=False)
+    url = Column(String(50),unique=True, nullable=False)
+    personajes = relationship('Personaje', back_populates='planeta_origen')
+    favoritos= relationship('Favoritos')
+
+
+class Personaje(Base):
+    __tablename__ ='personaje'
+    id = Column(Integer, primary_key=True)
+    nombre= Column(String(50), nullable=False)
+    especie= Column(String(50), nullable=False)
+    genero= Column(String(50), nullable=False)
+    planeta_origen_id= Column (Integer, ForeignKey(Planeta.id))
+    planeta_origen = relationship('Planeta', back_populates='personajes')
+
+
+class Favoritos(Base):
+    __tablename__='favoritos'
+    id = Column(Integer, primary_key=True)
+    usuario_id= Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    tipo= Column(String(50), nullable=False)
+    elemento_id= Column(Integer, nullable=False)
+    usuario= relationship('Usuario', back_populates='favoritos')
+    planetas = relationship('Planeta', back_populates='favoritos')
+
+
+
 
     def to_dict(self):
         return {}
